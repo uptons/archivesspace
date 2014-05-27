23687338131c1bc0f25fab0760bed196fee7e2bf
jambun <jambun@gmail.com>
2014-05-27 10:25:14 +1000

    make sure people can still create events

--- common/db/migrations/033_split_update_archival_record_permission_by_record_type_and_related_permissions_changes.rb
+++ common/db/migrations/033_split_update_archival_record_permission_by_record_type_and_related_permissions_changes.rb
@@ -67,6 +67,18 @@ Sequel.migration do
                                                               :user_mtime => Time.now)
       end
 
+      update_event_permission_id = self[:permission].filter(:permission_code => 'update_event_record').get(:id)
+      if update_event_permission_id.nil?
+        update_event_permission_id = self[:permission].insert(:permission_code => 'update_event_record',
+                                                              :description => 'The ability to create and modify event records',
+                                                              :level => 'repository',
+                                                              :created_by => 'admin',
+                                                              :last_modified_by => 'admin',
+                                                              :create_time => Time.now,
+                                                              :system_mtime => Time.now,
+                                                              :user_mtime => Time.now)
+      end
+
       update_archival_record_group_ids = self[:group_permission].filter(:permission_id => [delete_archival_record_permission_id, update_archival_record_permission_id]).select(:group_id).map {|row| row[:group_id]}.uniq
       update_archival_record_group_ids.each do |group_id|
         self[:group_permission].insert(:permission_id => update_accession_permission_id,
@@ -79,6 +91,11 @@ Sequel.migration do
                                        :group_id => group_id)
         self[:group_permission].insert(:permission_id => manage_vocabulary_permission_id,
                                        :group_id => group_id)
+
+        if self[:group_permission].filter(:permission_id => update_event_permission_id, :group_id => group_id).get(:id).nil?
+          self[:group_permission].insert(:permission_id => update_event_permission_id,
+                                         :group_id => group_id)
+        end
       end
 
       self[:permission].delete(:permission_id => update_archival_record_permission)
