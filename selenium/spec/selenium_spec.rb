a83d658deac2b33307194ced37eab2236fbf305a
jambun <jambun@gmail.com>
2014-05-28 11:08:48 +1000

    added some selenium tests

--- selenium/spec/selenium_spec.rb
+++ selenium/spec/selenium_spec.rb
@@ -3123,4 +3123,52 @@ describe "ArchivesSpace user interface" do
 
   end
 
+  describe "Permissions" do
+
+    before(:all) do
+      @perm_test_repo = "perm_test#{Time.now.to_i}_#{$$}"
+      (moo, @repo_uri) = create_test_repo(@perm_test_repo, "The name of the #{@perm_test_repo}")
+      (@archivist, @pass) = create_user
+      add_user_to_archivists(@archivist, @repo_uri)
+    end
+
+
+    after(:all) do
+      logout
+    end
+
+
+    it "allows archivists to edit major record types by default" do
+      login(@archivist, @pass)
+      select_repo(@perm_test_repo)
+      $driver.find_element(:link => 'Create').click
+      $driver.find_element(:link => 'Accession').click
+      $driver.find_element(:link => 'Create').click
+      $driver.find_element(:link => 'Resource').click
+      $driver.find_element(:link => 'Create').click
+      $driver.find_element(:link => 'Digital Object').click
+      logout
+    end
+
+
+    it "supports denying permission to edit Resources" do
+      login_as_admin
+      select_repo(@perm_test_repo)
+      $driver.find_element(:css, '.repo-container .btn.dropdown-toggle').click
+      $driver.find_element(:link, "Manage Groups").click
+
+      row = $driver.find_element_with_text('//tr', /repository-archivists/)
+      row.find_element(:link, 'Edit').click
+      $driver.find_element(:xpath, '//input[@id="update_resource_record"]').click
+      $driver.find_element(:css => 'button[type="submit"]').click
+      logout
+      login(@archivist, @pass)
+      select_repo(@perm_test_repo)
+      $driver.find_element(:link => 'Create').click
+      $driver.ensure_no_such_element(:link, "Resource")
+      logout
+    end
+  
+  end
+
 end
