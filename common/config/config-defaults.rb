bf4df78f2ee0140cc687882b5334e739d4751e37
root <root@testms20.library.rice.edu>
2015-03-20 13:26:33 -0500

    add config-defauts.rb

--- common/config/config-defaults.rb
+++ common/config/config-defaults.rb
@@ -118,7 +118,7 @@ AppConfig[:report_pdf_font_paths] = proc { ["#{AppConfig[:backend_url]}/reports/
 AppConfig[:report_pdf_font_family] = "\"DejaVu Sans\", sans-serif"
 
 # Plug-ins to load. They will load in the order specified
-AppConfig[:plugins] = ['local', 'aspace_feedback', 'aspace-public-formats']
+AppConfig[:plugins] = ['local', 'aspace_feedback', 'aspace-public-formats','aspace-search-identifier']
 
 
 # 
